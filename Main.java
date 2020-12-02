2608c3ba7b47629a4df38ecb65c69d985de6fb86
Dimitris Grigoratos <72795310+grigoratos@users.noreply.github.com>
2020-12-02 21:46:35 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -17,11 +17,11 @@ public static void main(String args[]) {
 			int x = intException();
 			System.out.println();
 			if (x == 1) {
-				Information.information()l
+				System.out.println("Soon to be added.");
 			} else if (x == 2) {
 				Patient.addPatient();
 			} else if (x == 3) {
-				Patient.printPatient();
+				System.out.println("Soon to be added.");
 			} else if (x == 4) {
 				System.out.println( "(1) Στατιστικά ανά Γεωγραφικό Διαμέρισμα\n"
 						+ "(2) Στατιστικά ανά Ημέρα Επιλογής\n"
