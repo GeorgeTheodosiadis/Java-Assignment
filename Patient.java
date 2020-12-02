b4dceb8a26205a7515cb500b14eb7714655d742d
GeorgeTheodosiadis <72859469+GeorgeTheodosiadis@users.noreply.github.com>
2020-12-02 18:28:14 +0200

    Update Patient.java

--- Patient.java
+++ Patient.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 /**
  * This class is used to track the Patients that are tested for Covid-19
  * and insert their firstName, lastName, gender, region, email, age,
@@ -9,29 +11,98 @@ import java.util.ArrayList;
  *
  */
 public class Patient {
-	private String firstName, lastName, gender, region, email, phoneNumber;
-	private int age, covidTestDay, covidTestMonth, covidTestYear;
+	private String firstName, lastName;
+	private String gender, region, email;
+	private int age, phoneNumber;
+	private int covidTestDay, covidTestMonth, covidTestYear;
 	private boolean infected;
-	public static ArrayList<Patient> myPatient = new ArrayList<Patient>();
+	public static ArrayList<Patient> myPatient=new ArrayList<Patient>();
 /**
  * 
  * This is the Constructor method of the class
  * 
  */
-	public Patient(String firstName, String lastName, String gender, String region, String email, int age,
-		       String phoneNumber, int covidTestDay, int covidTestMonth, int covidTestYear, boolean infected) {
+	public Patient(String fN,String lN,String gen,String reg,String em,int age,int pN,int cTD,int cTM,int cTY,boolean inf) {
 		myPatient.add(this);
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.gender = gender;
-		this.region = region;
-		this.email = email;
-		this.age = age;
-		this.phoneNumber = phoneNumber;
-		this.covidTestDay = covidTestDay;
-		this.covidTestMonth = covidTestMonth;
-		this.covidTestYear = covidTestYear;
-		this.infected = infected;
+		this.firstName=fN;
+		this.lastName=lN;
+		this.gender=gen;
+		this.region=reg;
+		this.email=em;
+		this.age=age;
+		this.phoneNumber=pN;
+		this.covidTestDay=cTD;
+		this.covidTestMonth=cTM;
+		this.covidTestYear=cTY;
+		this.infected=inf;
+	}
+
+	public void addPatient() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Εισάγετε το όνομά σας");
+		String firstName =  sc.next();
+		System.out.println("Εισάγετε το επίθετό σας");
+		String lastName = sc.next();
+		System.out.println("Εισάγετε το φύλο σας: άνδρας/γυναίκα");
+		String gender = sc.next();
+		System.out.println("Εισάγετε την περιοχή σας");
+		String region = sc.next();
+		System.out.println("Εισάγετε το email σας");
+		String email = sc.next();
+		int age = 0;
+		while (age <= 0 ) {
+			try {
+				System.out.println("Εισάγετε την ηλικία σας");
+				age = sc.nextInt();
+			}
+			catch (InputMismatchException e) {
+				System.out.println("Μη έγκυρη τιμή");
+			}
+		}
+		System.out.println("Εισάγετε το τηλέφωνό σας");
+		int phoneNumber = sc.nextInt();
+		System.out.println("Εισάγετε την ημερομηνία");
+		int covidTestDay = 0;
+		while (covidTestDay <= 0 && covidTestDay > 31 ) {
+			try {
+				System.out.println("Μέρα:");
+				covidTestDay = sc.nextInt();
+			}
+			catch (InputMismatchException e) {
+				System.out.println("Μη έγκυρη τιμή");
+			}
+		}
+		
+		int covidTestMonth = 0;
+		while (covidTestMonth <= 0 && covidTestMonth > 12 ) {
+			try {
+				System.out.println("Μήνας:");
+				covidTestMonth = sc.nextInt();
+			}
+			catch (InputMismatchException e) {
+				System.out.println("Μη έγκυρη τιμή");
+			}
+		}
+		int covidTestYear = 0;
+		while (covidTestYear <=0 ) {
+			try {
+				System.out.println("Έτος:");
+				age = sc.nextInt();
+			}
+			catch (InputMismatchException e) {
+				System.out.println("Μη έγκυρη τιμή");
+			}
+		}
+		
+		System.out.println("Εισάγετε το αποτέλεσμα του test: θετικό/αρνητικό");
+		String apantisi = sc.next();
+		boolean infected;
+		if (apantisi.equals("θετικό")) {
+			infected = true;
+		} else {
+			infected = false;
+		}
+		Patient Patient = new Patient(firstName, lastName, gender, region, email, age, phoneNumber, covidTestDay, covidTestMonth, covidTestYear, infected);
 	}
 /**
  * 
@@ -86,11 +157,11 @@ public class Patient {
 		this.age = age;
 	}
 
-	public String getPhoneNumber() {
+	public int getPhoneNumber() {
 		return phoneNumber;
 	}
 
-	public void setPhoneNumber(String phoneNumber) {
+	public void setPhoneNumber(int phoneNumber) {
 		this.phoneNumber = phoneNumber;
 	}
 
